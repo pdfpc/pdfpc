f856b03e0d18852133267ca44de1c37d5abc7c4b
Jakob Westhoff <jakob@westhoffswelt.de>
2009-12-14 22:06:23 +0000

    - Implemented: Build system now creates vapi and header files # This is needed for the test system which is about to be created

--- conf/FindVala.cmake
+++ conf/FindVala.cmake
@@ -67,7 +67,7 @@ endmacro(parse_arguments)
 # vala compiler.
 macro(vala_precompile output)
 	include_directories(${CMAKE_CURRENT_BINARY_DIR})
-	parse_arguments(ARGS "PACKAGES;OPTIONS" "" ${ARGN})
+	parse_arguments(ARGS "PACKAGES;OPTIONS;GENERATE_HEADER;GENERATE_VAPI" "" ${ARGN})
 	set(vala_pkg_opts "")
 	foreach(pkg ${ARGS_PACKAGES})
 		list(APPEND vala_pkg_opts "--pkg=${pkg}")
@@ -81,8 +81,23 @@ macro(vala_precompile output)
 		list(APPEND out_files "${CMAKE_CURRENT_BINARY_DIR}/${src}")
 		list(APPEND ${output} ${out_file})
 	endforeach(src ${ARGS_DEFAULT_ARGS})
+
+	set(header_arguments "")
+	if(ARGS_GENERATE_HEADER)
+		list(APPEND out_files "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_GENERATE_HEADER}.h")
+		list(APPEND header_arguments "-H")
+		list(APPEND header_arguments "${ARGS_GENERATE_HEADER}.h")
+	endif(ARGS_GENERATE_HEADER)
+
+	set(vapi_arguments "")
+	if(ARGS_GENERATE_VAPI)
+		list(APPEND out_files "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_GENERATE_VAPI}.vapi")
+		set(vapi_arguments "--library=${ARGS_GENERATE_VAPI}")
+	endif(ARGS_GENERATE_VAPI)
+
 	add_custom_command(OUTPUT ${out_files} 
-					   COMMAND ${VALA_EXECUTABLE} ARGS "-C" "-b" ${CMAKE_CURRENT_SOURCE_DIR} 
-					   "-d" ${CMAKE_CURRENT_BINARY_DIR} ${vala_pkg_opts} ${ARGS_OPTIONS} ${in_files}
+					   COMMAND ${VALA_EXECUTABLE} ARGS "-C" ${header_arguments} ${vapi_arguments}
+					   "-b" ${CMAKE_CURRENT_SOURCE_DIR} "-d" ${CMAKE_CURRENT_BINARY_DIR} 
+					   ${vala_pkg_opts} ${ARGS_OPTIONS} ${in_files}
 					   DEPENDS ${in_files})
 endmacro(vala_precompile)
