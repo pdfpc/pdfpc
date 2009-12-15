bedd8b962b0209a95e5404e3cb03323ba56084ff
Jakob Westhoff <jakob@westhoffswelt.de>
2009-12-15 21:20:11 +0000

    - Refactored vala cmake macros as a first step to export them to an external   repository

--- conf/FindVala.cmake
+++ conf/FindVala.cmake
@@ -1,15 +1,10 @@
-##
-# The Vala Macros have been created by Florian Sowade
-# Thanks for your help :)
-##
-
 # - Find vala compiler
 # This module finds if vala compiler is installed and determines where the
 # executables are. This code sets the following variables:
 #
 #  VALA_FOUND       - Was the vala compiler found
 #  VALA_EXECUTABLE  - path to the vala compiler
-#
+#  VALA_VERSION     - The version number of the available valac
 
 find_program(VALA_EXECUTABLE
   NAMES valac)
@@ -26,80 +21,3 @@ execute_process(COMMAND ${VALA_EXECUTABLE} "--version"
                 OUTPUT_VARIABLE "VALA_VERSION")
 string(REPLACE "Vala" "" "VALA_VERSION" ${VALA_VERSION})
 string(STRIP ${VALA_VERSION} "VALA_VERSION")
-
-# This is a helper Macro to parse optional arguments in Macros/Functions
-# See http://www.cmake.org/Wiki/CMakeMacroParseArguments for documentation
-macro(parse_arguments prefix arg_names option_names)
-  set(DEFAULT_ARGS)
-  foreach(arg_name ${arg_names})
-    set(${prefix}_${arg_name})
-  endforeach(arg_name)
-  foreach(option ${option_names})
-    set(${prefix}_${option} FALSE)
-  endforeach(option)
-
-  set(current_arg_name DEFAULT_ARGS)
-  set(current_arg_list)
-  foreach(arg ${ARGN})
-    set(larg_names ${arg_names})
-    list(FIND larg_names "${arg}" is_arg_name)
-    if(is_arg_name GREATER -1)
-      set(${prefix}_${current_arg_name} ${current_arg_list})
-      set(current_arg_name ${arg})
-      set(current_arg_list)
-    else(is_arg_name GREATER -1)
-      set(loption_names ${option_names})
-      list(FIND loption_names "${arg}" is_option)
-      if(is_option GREATER -1)
-	    set(${prefix}_${arg} TRUE)
-      else(is_option GREATER -1)
-	    set(current_arg_list ${current_arg_list} ${arg})
-      endif(is_option GREATER -1)
-    endif(is_arg_name GREATER -1)
-  endforeach(arg)
-  set(${prefix}_${current_arg_name} ${current_arg_list})
-endmacro(parse_arguments)
-
-# vala_precompile[output src.vala ... [PACKAGES ...] [OPTIONS ...]]
-# This macro precomiples the given vala files to .c files and puts
-# a list with the generated .c files to output.
-# packages and additional compiler options are directly passed to the
-# vala compiler.
-macro(vala_precompile output)
-	include_directories(${CMAKE_CURRENT_BINARY_DIR})
-	parse_arguments(ARGS "PACKAGES;OPTIONS;GENERATE_HEADER;GENERATE_VAPI;CUSTOM_VAPIS" "" ${ARGN})
-	set(vala_pkg_opts "")
-	foreach(pkg ${ARGS_PACKAGES})
-		list(APPEND vala_pkg_opts "--pkg=${pkg}")
-	endforeach(pkg ${ARGS_PACKAGES})
-	set(in_files "")
-	set(out_files "")
-	set(${output} "")
-	foreach(src ${ARGS_DEFAULT_ARGS})
-		list(APPEND in_files "${CMAKE_CURRENT_SOURCE_DIR}/${src}")
-		string(REPLACE ".vala" ".c" src ${src})
-		set(out_file "${CMAKE_CURRENT_BINARY_DIR}/${src}")
-		list(APPEND out_files "${CMAKE_CURRENT_BINARY_DIR}/${src}")
-		list(APPEND ${output} ${out_file})
-	endforeach(src ${ARGS_DEFAULT_ARGS})
-
-	set(header_arguments "")
-	if(ARGS_GENERATE_HEADER)
-		list(APPEND out_files "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_GENERATE_HEADER}.h")
-		list(APPEND out_files "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_GENERATE_HEADER}_internal.h")
-		list(APPEND header_arguments "--header=${ARGS_GENERATE_HEADER}.h")
-		list(APPEND header_arguments "--internal-header=${ARGS_GENERATE_HEADER}_internal.h")
-	endif(ARGS_GENERATE_HEADER)
-
-	set(vapi_arguments "")
-	if(ARGS_GENERATE_VAPI)
-		list(APPEND out_files "${CMAKE_CURRENT_BINARY_DIR}/${ARGS_GENERATE_VAPI}.vapi")
-		set(vapi_arguments "--internal-vapi=${ARGS_GENERATE_VAPI}.vapi")
-	endif(ARGS_GENERATE_VAPI)
-
-	add_custom_command(OUTPUT ${out_files} 
-					   COMMAND ${VALA_EXECUTABLE} ARGS "-C" ${header_arguments} ${vapi_arguments}
-					   "-b" ${CMAKE_CURRENT_SOURCE_DIR} "-d" ${CMAKE_CURRENT_BINARY_DIR} 
-					   ${vala_pkg_opts} ${ARGS_OPTIONS} ${in_files} ${ARGS_CUSTOM_VAPIS}
-					   DEPENDS ${in_files} ${ARGS_CUSTOM_VAPIS})
-endmacro(vala_precompile)
