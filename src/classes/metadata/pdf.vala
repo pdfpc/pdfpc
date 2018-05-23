pdfpc/pdfpc

commit 13e3b372711fdc94acdb26be69ef0cf1465911ed

Revert to using render() instead of render_for_printing_with_options() (#344)
Revert to using render() instead of render_for_printing_with_options()
to avoid inferior quality of Cairo rendering in this mode. To prevent
displaying PDF annotations, remove the programmatically.

Add version dependency:
PopplerPage.remove_anot() is available since 0.22

src/classes/metadata/pdf.vala
@@ -444,6 +444,8 @@ namespace pdfpc.Metadata {
                             note_text += a.get_contents();
                             break;
                     }
+                    // Remove the annotation to avoid its rendering
+                    page.remove_annot(a);
                 }
                 if (note_text != "") {
                     this.notes.set_note(note_text, user_slide, true);

src/classes/renderer/pdf.vala
@@ -120,8 +120,7 @@ namespace pdfpc {
             cr.scale(this.scaling_factor, this.scaling_factor);
             cr.translate(-metadata.get_horizontal_offset(this.area),
                 -metadata.get_vertical_offset(this.area));
-            page.render_for_printing_with_options(cr,
-                Poppler.PrintFlags.DOCUMENT);
+            page.render(cr);
 
             // If the cache is enabled store the newly rendered pixmap
             if (this.cache != null) {

src/CMakeLists.txt
@@ -2,7 +2,7 @@ find_package(PkgConfig)
 pkg_check_modules(GOBJECT REQUIRED gobject-2.0)
 pkg_check_modules(GIO REQUIRED gio-2.0)
 pkg_check_modules(GEE REQUIRED gee-0.8)
-pkg_check_modules(POPPLER REQUIRED poppler-glib)
+pkg_check_modules(POPPLER REQUIRED poppler-glib>=0.22)
 pkg_check_modules(GTK REQUIRED gtk+-3.0>=3.10)
 list (FIND GTK_STATIC_LIBRARIES "X11" _index)
 if (${_index} GREATER -1)

commit 21e4efb3afe325fe7e2f800d1c22fd1bc28bc3d7

Fix regression introduced in 13e3b372711fdc94acdb26be69ef0cf1465911ed
See Debian Bug 888677.

src/classes/metadata/pdf.vala
@@ -442,10 +442,12 @@ namespace pdfpc.Metadata {
                                 note_text += "\n";
                             }
                             note_text += a.get_contents();
+
+                            // Remove the annotation to avoid its rendering
+                            page.remove_annot(a);
+
                             break;
                     }
-                    // Remove the annotation to avoid its rendering
-                    page.remove_annot(a);
                 }
                 if (note_text != "") {
                     this.notes.set_note(note_text, user_slide, true);