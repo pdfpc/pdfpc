pdfpc/pdfpc

commit 424d72fb4889d03948f73e577a5198b7fa21fbe7

Render "normal" and beamer notes in the same place
This implements #479 (and should also close #353).

src/classes/action/movie.vala
@@ -675,14 +675,8 @@ namespace pdfpc {
                 n++;
             }
 
-            bool notes_mode = (Options.notes_position != null) ? true : false;
             n = 0;
             foreach (var conf in video_confs) {
-                // if --notes passed, hide the video on the presenter screen
-                if (notes_mode && conf.window.is_presenter) {
-                    continue;
-                }
-
                 Gst.Element sink;
                 try {
                     sink = gst_element_make("gtksink", @"sink$n");
@@ -697,7 +691,7 @@ namespace pdfpc {
                 Gst.Element queue = Gst.ElementFactory.make("queue", @"queue$n");
                 bin.add_many(queue, sink);
                 tee.link(queue);
-                if ((conf.window.is_presenter && !notes_mode) ||
+                if ((conf.window.is_presenter) ||
                     !conf.window.is_presenter) {
                     Gst.Element ad_element = this.add_video_control(queue, bin,
                         conf.rect);
@@ -733,8 +727,7 @@ namespace pdfpc {
 
                         // Update the rectangle
                         conf.rect = rect;
-                        if ((window.is_presenter && !notes_mode) ||
-                            (!window.is_presenter && notes_mode)) {
+                        if (window.is_presenter) {
                             this.rect = rect;
                             this.scalex = (double) this.video_w/rect.width;
                             this.scaley = (double) this.video_h/rect.height;

src/classes/metadata/pdf.vala
@@ -916,7 +916,7 @@ namespace pdfpc.Metadata {
         /**
          * Return the width of the first page of the loaded pdf document.
          *
-         * If slides contains also notes, this method returns the width of the content part only
+         * If slides also contain notes, return the width of the content part only
          *
          * In presentations all pages will have the same size in most cases,
          * therefore this value is assumed to be useful.
@@ -926,7 +926,7 @@ namespace pdfpc.Metadata {
         }
 
         /**
-         * Fixes the page width if pdfpc uses notes in slit mode
+         * Fixes the page width if pdfpc uses notes in split mode
          */
          public double get_corrected_page_width(double page_width) {
             if (    this.notes_position == NotesPosition.LEFT
@@ -940,7 +940,7 @@ namespace pdfpc.Metadata {
         /**
          * Return the height of the first page of the loaded pdf document.
          *
-         * If slides contains also notes, this method returns the height of the content part only
+         * If slides also contain notes, return the height of the content part only
          *
          * In presentations all pages will have the same size in most cases,
          * therefore this value is assumed to be useful.
@@ -950,7 +950,7 @@ namespace pdfpc.Metadata {
         }
 
         /**
-         * Fixes the page height if pdfpc uses notes in slit mode
+         * Fixes the page height if pdfpc uses notes in split mode
          */
          public double get_corrected_page_height(double page_height) {
             if (    this.notes_position == NotesPosition.TOP
@@ -964,56 +964,52 @@ namespace pdfpc.Metadata {
         /**
          * Return the horizontal offset of the given area on the page
          */
-        public double get_horizontal_offset(Area area, double page_width = 0) {
+        public double get_horizontal_offset(bool notes_area,
+            double page_width = 0) {
             if (page_width == 0) {
                 page_width = this.original_page_width;
             }
 
-            switch (area) {
-                case Area.CONTENT:
-                    switch (this.notes_position) {
-                        case NotesPosition.LEFT:
-                            return page_width / 2;
-                        default:
-                            return 0;
-                    }
-                case Area.NOTES:
-                    switch (this.notes_position) {
-                        case NotesPosition.RIGHT:
-                            return page_width / 2;
-                        default:
-                            return 0;
-                    }
-                default:
-                    return 0;
+            if (notes_area) {
+                switch (this.notes_position) {
+                    case NotesPosition.RIGHT:
+                        return page_width / 2;
+                    default:
+                        return 0;
+                }
+            } else {
+                switch (this.notes_position) {
+                    case NotesPosition.LEFT:
+                        return page_width / 2;
+                    default:
+                        return 0;
+                }
             }
         }
 
         /**
          * Return the vertical offset of the given area on the page
          */
-        public double get_vertical_offset(Area area, double page_height = 0) {
+        public double get_vertical_offset(bool notes_area,
+            double page_height = 0) {
             if (page_height == 0) {
                 page_height = this.original_page_height;
             }
 
-            switch (area) {
-                case Area.CONTENT:
-                    switch (this.notes_position) {
-                        case NotesPosition.TOP:
-                            return page_height / 2;
-                        default:
-                            return 0;
-                    }
-                case Area.NOTES:
-                    switch (this.notes_position) {
-                        case NotesPosition.BOTTOM:
-                            return page_height / 2;
-                        default:
-                            return 0;
-                    }
-                default:
-                    return 0;
+            if (notes_area) {
+                switch (this.notes_position) {
+                    case NotesPosition.BOTTOM:
+                        return page_height / 2;
+                    default:
+                        return 0;
+                }
+            } else {
+                switch (this.notes_position) {
+                    case NotesPosition.TOP:
+                        return page_height / 2;
+                    default:
+                        return 0;
+                }
             }
         }
 
@@ -1134,15 +1130,12 @@ namespace pdfpc.Metadata {
             }
             return this.action_mapping;
         }
-    }
 
-    /**
-     * Defines an area on a pdf page
-     */
-    public enum Area {
-        FULL,
-        CONTENT,
-        NOTES;
+        public bool has_beamer_notes {
+            get {
+                return (this.notes_position != NotesPosition.NONE);
+            }
+        }
     }
 
     /**

src/classes/renderer/cache.vala
@@ -170,11 +170,13 @@ namespace pdfpc.Renderer {
         public uint index;
         public uint width;
         public uint height;
+        public bool notes_area;
 
-        public CachedPageProps(uint index, uint width, uint height) {
-            this.index  = index;
-            this.width  = width;
-            this.height = height;
+        public CachedPageProps(uint index, uint width, uint height, bool notes_area) {
+            this.index      = index;
+            this.width      = width;
+            this.height     = height;
+            this.notes_area = notes_area;
         }
 
         protected uint hash() {
@@ -182,9 +184,10 @@ namespace pdfpc.Renderer {
         }
 
         protected bool equal_to(CachedPageProps other) {
-            return this.index == other.index &&
-                   this.width == other.width &&
-                   this.height == other.height;
+            return this.index      == other.index  &&
+                   this.width      == other.width  &&
+                   this.height     == other.height &&
+                   this.notes_area == other.notes_area;
         }
     }
 

src/classes/renderer/pdf.vala
@@ -60,7 +60,7 @@ namespace pdfpc {
          * RenderError.SLIDE_DOES_NOT_EXIST error is thrown.
          */
         public Cairo.ImageSurface render(int slide_number,
-            Metadata.Area area, int width, int height,
+            bool notes_area, int width, int height,
             bool force_cache = false, bool permanent_cache = false,
             PresentationController.ScaledRectangle? zoom_area = null)
             throws Renderer.RenderError {
@@ -77,7 +77,8 @@ namespace pdfpc {
 
             // We never cache a zoomed-in slide
             if (zoom_area == null) {
-                props = new CachedPageProps(slide_number, width, height);
+                props = new CachedPageProps(slide_number,
+                    width, height, notes_area);
 
                 // Check for the page in the cache
                 Cairo.ImageSurface cache_content;
@@ -138,8 +139,8 @@ namespace pdfpc {
 
             cr.scale(scaling_factor, scaling_factor);
 
-            cr.translate(-metadata.get_horizontal_offset(area, full_page_width) + h_offset,
-                -metadata.get_vertical_offset(area, full_page_height) + v_offset);
+            cr.translate(-metadata.get_horizontal_offset(notes_area, full_page_width) + h_offset,
+                -metadata.get_vertical_offset(notes_area, full_page_height) + v_offset);
 
             page.render(cr);
 

src/classes/view/pdf.vala
@@ -101,9 +101,9 @@ namespace pdfpc {
         protected int gdk_scale = 1;
 
         /**
-         * The area of the pdf which shall be displayed
+         * Whether the PDF area to be displayed is (beamer) notes
          */
-        protected Metadata.Area area;
+        protected bool notes_area;
 
         /**
          * ID of timeout used to delay pre-rendering
@@ -178,7 +178,7 @@ namespace pdfpc {
                     // pixmap in the cache if it is enabled. This
                     // is exactly what we want.
                     try {
-                        this.renderer.render(*p_slide, this.area, width, height,
+                        this.renderer.render(*p_slide, this.notes_area, width, height,
                             true);
                     } catch(Renderer.RenderError e) {
                         GLib.printerr("Could pre-render page '%i': %s\n",
@@ -204,12 +204,12 @@ namespace pdfpc {
         /**
          * Default constructor restricted to Pdf renderers as input parameter
          */
-        protected Pdf(Renderer.Pdf renderer, Metadata.Area area,
+        protected Pdf(Renderer.Pdf renderer, bool notes_area,
             bool clickable_links, PresentationController controller,
             int gdk_scale_factor, bool user_slides) {
             this.renderer = renderer;
             this.gdk_scale = gdk_scale_factor;
-            this.area = area;
+            this.notes_area = notes_area;
             this.user_slides = user_slides;
 
             this.current_slide_number = -1;
@@ -229,13 +229,13 @@ namespace pdfpc {
          * metadata and rendering chain to be used with the pdf view.
          */
         public Pdf.from_fullscreen(Window.Fullscreen window,
-            Metadata.Area area, bool clickable_links, bool user_slides=false) {
+            bool notes_area, bool clickable_links, bool user_slides=false) {
             var controller = window.controller;
             var metadata = controller.metadata;
 
             var renderer = metadata.renderer;
 
-            this(renderer, area, clickable_links, controller, window.gdk_scale,
+            this(renderer, notes_area, clickable_links, controller, window.gdk_scale,
                 user_slides);
         }
 
@@ -409,7 +409,7 @@ namespace pdfpc {
                     if (this.current_slide_number < this.n_slides && !this.disabled) {
                         this.current_slide =
                             this.renderer.render(this.current_slide_number,
-                                this.area, width, height,
+                                this.notes_area, width, height,
                                 false, false, this.zoom_area);
 
                         if (Options.prerender_slides != 0) {

src/classes/window/overview.vala
@@ -363,7 +363,7 @@ namespace pdfpc.Window {
                 var real_slide_id =
                     metadata.user_slide_to_real_slide(this.slide_id, true);
                 slide_to_fill = renderer.render(real_slide_id,
-                    Metadata.Area.CONTENT, slide_width, slide_height,
+                    false, slide_width, slide_height,
                     true, true);
             } catch (Renderer.RenderError e) {
                 ;

src/classes/window/presentation.vala
@@ -55,8 +55,7 @@ namespace pdfpc.Window {
             this.controller.update_request.connect(this.update);
             this.controller.zoom_request.connect(this.on_zoom);
 
-            this.view = new View.Pdf.from_fullscreen(this,
-                Metadata.Area.CONTENT, true);
+            this.view = new View.Pdf.from_fullscreen(this, false, true);
             this.view.transitions_enabled = true;
 
             this.overlay_layout.add(this.view);

src/classes/window/presenter.vala
@@ -48,7 +48,7 @@ namespace pdfpc.Window {
         /**
          * View showing the current slide
          */
-        public View.Pdf current_view;
+        protected View.Pdf current_view;
 
         /**
          * View showing a preview of the next slide
@@ -60,6 +60,14 @@ namespace pdfpc.Window {
          */
         protected View.Pdf strict_next_view;
         protected View.Pdf strict_prev_view;
+
+        /**
+         * A stack of text box (for editing) and rendered view of notes
+         * for the current slide
+         */
+        protected Gtk.Stack notes_stack;
+        protected Gtk.TextView notes_editor;
+        protected View.Pdf notes_view;
 
         /**
          * Timer for the presenation
@@ -115,11 +123,6 @@ namespace pdfpc.Window {
          * Indication that the notes are read-only (coming from PDF annotations)
          */
         protected Gtk.Image locked_icon;
-
-        /**
-         * Text box for displaying notes for the slides
-         */
-        protected Gtk.TextView notes_view;
 
         /**
          * CSS provider for setting note font size
@@ -479,28 +482,35 @@ namespace pdfpc.Window {
             int current_allocated_width = (int) Math.floor(
                 this.window_w*Options.current_size/100.0);
             this.current_view = new View.Pdf.from_fullscreen(this,
-                Metadata.Area.NOTES, true);
+                false, true);
 
             this.next_view = new View.Pdf.from_fullscreen(this,
-                Metadata.Area.CONTENT, false, true);
+                false, false, true);
 
             this.strict_next_view = new View.Pdf.from_fullscreen(this,
-                Metadata.Area.CONTENT, false);
+                false, false);
             this.strict_prev_view = new View.Pdf.from_fullscreen(this,
-                Metadata.Area.CONTENT, false);
+                false, false);
 
             this.css_provider = new Gtk.CssProvider();
             Gtk.StyleContext.add_provider_for_screen(this.screen_to_use,
-                css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER);
+                this.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER);
+
+            this.bottom_text_css_provider = new Gtk.CssProvider();
+            Gtk.StyleContext.add_provider_for_screen(this.screen_to_use,
+                this.bottom_text_css_provider,
+                Gtk.STYLE_PROVIDER_PRIORITY_FALLBACK);
+
+            Gtk.AspectFrame frame;
 
             // TextView for notes in the slides
-            this.notes_view = new Gtk.TextView();
-            this.notes_view.name = "notesView";
-            this.notes_view.editable = false;
-            this.notes_view.cursor_visible = false;
-            this.notes_view.wrap_mode = Gtk.WrapMode.WORD;
-            this.notes_view.buffer.text = "";
-            this.notes_view.key_press_event.connect(this.on_key_press_notes_view);
+            this.notes_editor = new Gtk.TextView();
+            this.notes_editor.name = "notesView";
+            this.notes_editor.editable = false;
+            this.notes_editor.cursor_visible = false;
+            this.notes_editor.wrap_mode = Gtk.WrapMode.WORD;
+            this.notes_editor.buffer.text = "";
+            this.notes_editor.key_press_event.connect(this.on_key_press_notes_editor);
             if (this.metadata.font_size >= 0) {
                 // LEGACY font size detection
                 // Before, we had the font size in absolute (device) units.
@@ -510,11 +520,19 @@ namespace pdfpc.Window {
                 }
                 this.set_font_size(this.metadata.font_size);
             }
+            var notes_sw = new Gtk.ScrolledWindow(null, null);
+            notes_sw.add(this.notes_editor);
+            notes_sw.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC);
 
-            this.bottom_text_css_provider = new Gtk.CssProvider();
-            Gtk.StyleContext.add_provider_for_screen(this.screen_to_use,
-                this.bottom_text_css_provider,
-                Gtk.STYLE_PROVIDER_PRIORITY_FALLBACK);
+            this.notes_view = new View.Pdf.from_fullscreen(this,
+                true, false);
+            frame = new Gtk.AspectFrame(null, 0.5f, 0.0f, page_ratio, false);
+            frame.add(this.notes_view);
+
+            this.notes_stack = new Gtk.Stack();
+            this.notes_stack.add_named(notes_sw, "editor");
+            this.notes_stack.add_named(frame, "view");
+            this.notes_stack.homogeneous = true;
 
             // The countdown timer is centered in the 90% bottom part of the screen
             this.timer = this.controller.getTimer();
@@ -550,8 +568,6 @@ namespace pdfpc.Window {
             this.overview.hexpand = true;
             this.overview.set_n_slides(this.controller.user_n_slides);
             this.controller.set_overview(this.overview);
-
-            Gtk.AspectFrame frame;
 
             var slide_views = new Gtk.Paned(Gtk.Orientation.HORIZONTAL);
             slide_views.position = current_allocated_width;
@@ -610,10 +626,7 @@ namespace pdfpc.Window {
             frame.add(next_view);
             next_view_and_notes.pack1(frame, true, true);
 
-            var notes_sw = new Gtk.ScrolledWindow(null, null);
-            notes_sw.add(this.notes_view);
-            notes_sw.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC);
-            next_view_and_notes.pack2(notes_sw, true, true);
+            next_view_and_notes.pack2(this.notes_stack, true, true);
             slide_views.pack2(next_view_and_notes, true, true);
 
             var help_sw = create_help_window();
@@ -912,11 +925,12 @@ namespace pdfpc.Window {
             this.next_view.invalidate();
             this.strict_next_view.invalidate();
             this.strict_prev_view.invalidate();
+            this.notes_view.invalidate();
             this.overview.set_n_slides(this.controller.user_n_slides);
         }
 
         public void update() {
-            if (!metadata.is_ready) {
+            if (!this.metadata.is_ready) {
                 return;
             }
             int current_slide_number = this.controller.current_slide_number;
@@ -944,6 +958,13 @@ namespace pdfpc.Window {
                 this.strict_prev_view.disabled = true;
             }
             this.strict_prev_view.display(current_slide_number - 1);
+
+            if (this.metadata.has_beamer_notes) {
+                this.notes_stack.set_visible_child_name("view");
+                this.notes_view.display(current_slide_number);
+            } else {
+                this.notes_stack.set_visible_child_name("editor");
+            }
 
             this.update_slide_count();
             this.update_note();
@@ -1012,7 +1033,7 @@ namespace pdfpc.Window {
         }
 
         /**
-         * Edit a note. Basically give focus to notes_view
+         * Edit a note. Basically give focus to notes_editor
          */
         public void edit_note() {
             // Ignore events coming from the presentation view
@@ -1022,25 +1043,26 @@ namespace pdfpc.Window {
 
             // Disallow editing notes imported from PDF annotations
             int number = this.controller.current_user_slide_number;
-            if (this.metadata.get_notes().is_note_read_only(number)) {
+            if (this.metadata.get_notes().is_note_read_only(number) ||
+                this.metadata.has_beamer_notes) {
                 blink_lock_icon();
                 return;
             }
 
-            this.notes_view.editable = true;
-            this.notes_view.cursor_visible = true;
-            this.notes_view.grab_focus();
+            this.notes_editor.editable = true;
+            this.notes_editor.cursor_visible = true;
+            this.notes_editor.grab_focus();
             this.controller.set_ignore_input_events(true);
         }
 
         /**
          * Handle key presses when editing a note
          */
-        protected bool on_key_press_notes_view(Gtk.Widget source, Gdk.EventKey key) {
+        protected bool on_key_press_notes_editor(Gtk.Widget source, Gdk.EventKey key) {
             if (key.keyval == Gdk.Key.Escape) { /* Escape */
-                this.notes_view.editable = false;
-                this.notes_view.cursor_visible = false;
-                this.metadata.get_notes().set_note(this.notes_view.buffer.text,
+                this.notes_editor.editable = false;
+                this.notes_editor.cursor_visible = false;
+                this.metadata.get_notes().set_note(this.notes_editor.buffer.text,
                     this.controller.current_user_slide_number);
                 this.controller.set_ignore_input_events(false);
                 return true;
@@ -1055,7 +1077,7 @@ namespace pdfpc.Window {
         protected void update_note() {
             string this_note = this.metadata.get_notes().get_note_for_slide(
                 this.controller.current_user_slide_number);
-            this.notes_view.buffer.text = this_note;
+            this.notes_editor.buffer.text = this_note;
         }
 
         public void show_overview() {
@@ -1101,7 +1123,7 @@ namespace pdfpc.Window {
         }
 
         private int get_font_size() {
-            Gtk.StyleContext style_context = this.notes_view.get_style_context();
+            Gtk.StyleContext style_context = this.notes_editor.get_style_context();
             Pango.FontDescription font_desc;
             style_context.get(style_context.get_state(), "font", out font_desc, null);
 

commit 859e707a5827ea4e1dd5a4715ec37eff5920a513

Fix double attempt to create video controls
The bug was introduced in 424d72fb4889d03948f73e577a5198b7fa21fbe7

src/classes/action/movie.vala
@@ -691,8 +691,7 @@ namespace pdfpc {
                 Gst.Element queue = Gst.ElementFactory.make("queue", @"queue$n");
                 bin.add_many(queue, sink);
                 tee.link(queue);
-                if ((conf.window.is_presenter) ||
-                    !conf.window.is_presenter) {
+                if (conf.window.is_presenter) {
                     Gst.Element ad_element = this.add_video_control(queue, bin,
                         conf.rect);
                     ad_element.link(sink);